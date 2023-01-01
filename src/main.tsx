9659ebf96fb89769b182deeb42b36187ae9b7756
Grace de la Mora <thetrend@users.noreply.github.com>
2023-01-01 15:50:11 -0500

    fix: remove Auth0 from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-import { Auth0Provider } from "@auth0/auth0-react";
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Auth0Provider
-      domain="graced-is.us.auth0.com"
-      clientId="kX9nfyM9yLVkqrCpc3lB02pLPbqKjwFA"
-      redirectUri={window.location.origin}
-    >
-      <App />
-    </Auth0Provider>
+    <App />
   </React.StrictMode>,
 );
