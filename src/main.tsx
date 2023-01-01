e895c7850dbd32042e4e7f5d084555f4c66ed817
Grace de la Mora <thetrend@users.noreply.github.com>
2023-01-01 12:07:23 -0500

    chore: set up login button with auth0

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="graced-is.us.auth0.com"
+      clientId="kX9nfyM9yLVkqrCpc3lB02pLPbqKjwFA"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
-)
+);
